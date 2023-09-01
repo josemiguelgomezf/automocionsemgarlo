bd1932c050b5b1a67da6197477c131dbcc5fcad3
Jose Miguel <josemiguel.gmezfdez@gmail.com>
2023-09-01 15:44:25 +0200

    01/09/2023 - Pruebas useState

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,9 +1,15 @@
 import './MyButton.css';
+import { useState } from 'react';
 
 function MyButton() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
+        alert('You clicked me!');
+      }
     return (
-      <button>
-        I'm a button
+      <button onClick={handleClick}>
+        I'm a button, clicked {count} times.
       </button>
     );
   }
--- src/Reviews.js
+++ src/Reviews.js
@@ -2,11 +2,13 @@ const users = [{
     name: 'Hedy Lamarr',
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
     imageSize: 90,
+    isClient: true
   },
   {
     name: 'Hedy Lamarr2',
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
     imageSize: 90,
+    isClient: false
   }];
 
 function getRandomInt(max) {
@@ -18,7 +20,11 @@ let user=users[getRandomInt(2)];
 function Reviews() {
     return (
       <>
-        <h1>{user.name}</h1>
+        <h1
+            style={{
+                color: user.isClient ? 'magenta' : 'darkgreen'
+              }}
+        >{user.name}</h1>
         <img
           className="avatar"
           src={user.imageUrl}
