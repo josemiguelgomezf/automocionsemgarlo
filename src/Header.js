4443a5a096036ada3aeb5b0da37f95d778b2b75d
José Miguel Gómez Fernández <91533325+josemiguelgomezf@users.noreply.github.com>
2023-09-08 11:16:16 +0200

    08/09/2023 Actualizamos el Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,22 @@ import logo from './logo.png';
 
 function Header() {
     return (
-        <div>
-            <img src={logo} className="header-logo" alt="logo" />
-            <p>Stay Strong</p>
+    <div id="header">
+        <div id="header-pack-logo">
+            <img src="https://static.vecteezy.com/system/resources/previews/000/595/983/non_2x/vector-object-and-icons-for-sport-label-gym-badge-fitness-logo-design.jpg"id="header-logo" alt="logo" />
+            <p id="header-title">Stay Strong</p>
         </div>
+        <div id="header-menu">
+            <p><a href="#">Home</a></p>
+            <p><a href="#">About</a></p>
+            <p><a href="#">Services</a></p>
+            <p><a href="#">Contact</a></p>
+            <button>Click Me!</button>
+            <button>Click Me!</button>
+            <img src="https://cdn-icons-png.flaticon.com/512/3059/3059561.png" className="header-icon" alt="call" />
+            <img src="https://assets.stickpng.com/thumbs/5a4e2ef62da5ad73df7efe6e.png" className="header-icon" alt="whatsapp" />
+        </div>
+    </div>
     );
   }
 
